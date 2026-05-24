42696a59ffeb463c9e3ba85964ca7dd40872f245
Yuliia Kushnir <lavrinenkoyuli@gmail.com>
2026-05-24 08:50:25 +0300

    added jwt decoder

--- src/main/java/org/example/userservice/config/SecurityConfig.java
+++ src/main/java/org/example/userservice/config/SecurityConfig.java
@@ -10,6 +10,11 @@ import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationConverter;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.oauth2.jwt.JwtDecoder;
+import org.springframework.security.oauth2.jwt.NimbusJwtDecoder;
+
+import javax.net.ssl.*;
+import java.security.cert.X509Certificate;
 
 import java.util.List;
 import java.util.Map;
@@ -24,32 +29,32 @@ public class SecurityConfig {
         return http
                 .csrf(csrf -> csrf.disable())
                 .authorizeHttpRequests(auth -> auth
-                                .anyRequest().permitAll()
+//                                .anyRequest().permitAll()
 
-//                        .requestMatchers("/api/users/register").permitAll()
-//                        .requestMatchers("/api/users/*/validate").permitAll()
-//
-//                        .requestMatchers(HttpMethod.GET, "/api/users/**")
-//                        .hasAnyAuthority("ROLE_MANAGER", "ROLE_EXECUTOR", "ROLE_ADMIN")
-//
-//                        .requestMatchers(HttpMethod.GET, "/api/users/role/**")
-//                        .hasAnyAuthority("ROLE_MANAGER", "ROLE_ADMIN")
-//
-//                        .requestMatchers(HttpMethod.GET, "/api/users/managers/emails")
-//                        .hasAnyAuthority("ROLE_MANAGER", "ROLE_EXECUTOR", "ROLE_ADMIN")
-//
-//                        .requestMatchers(HttpMethod.PUT, "/api/users/*/personal-data")
-//                        .authenticated()
-//
-//                        .requestMatchers(HttpMethod.PUT, "/api/users/*/role")
-//                        .hasAuthority("ROLE_ADMIN")
-//
-//                        .requestMatchers(HttpMethod.DELETE, "/api/users/**")
-//                        .hasAuthority("ROLE_ADMIN")
-//
-//                        .requestMatchers("/actuator/**").permitAll()
-//
-//                        .anyRequest().authenticated()
+                        .requestMatchers("/api/users/register").permitAll()
+                        .requestMatchers("/api/users/*/validate").permitAll()
+
+                        .requestMatchers(HttpMethod.GET, "/api/users/**")
+                        .hasAnyAuthority("ROLE_MANAGER", "ROLE_EXECUTOR", "ROLE_ADMIN")
+
+                        .requestMatchers(HttpMethod.GET, "/api/users/role/**")
+                        .hasAnyAuthority("ROLE_MANAGER", "ROLE_ADMIN")
+
+                        .requestMatchers(HttpMethod.GET, "/api/users/managers/emails")
+                        .hasAnyAuthority("ROLE_MANAGER", "ROLE_EXECUTOR", "ROLE_ADMIN")
+
+                        .requestMatchers(HttpMethod.PUT, "/api/users/*/personal-data")
+                        .authenticated()
+
+                        .requestMatchers(HttpMethod.PUT, "/api/users/*/role")
+                        .hasAuthority("ROLE_ADMIN")
+
+                        .requestMatchers(HttpMethod.DELETE, "/api/users/**")
+                        .hasAuthority("ROLE_ADMIN")
+
+                        .requestMatchers("/actuator/**").permitAll()
+
+                        .anyRequest().authenticated()
                 )
                 .oauth2ResourceServer(oauth2 ->
                         oauth2.jwt(jwt -> jwt.jwtAuthenticationConverter(jwtAuthenticationConverter()))
@@ -79,5 +84,27 @@ public class SecurityConfig {
         });
 
         return converter;
+    }
+
+    @Bean
+    public JwtDecoder jwtDecoder() throws Exception {
+
+        TrustManager[] trustAllCerts = new TrustManager[]{
+                new X509TrustManager() {
+                    public X509Certificate[] getAcceptedIssuers() { return null; }
+                    public void checkClientTrusted(X509Certificate[] certs, String authType) {}
+                    public void checkServerTrusted(X509Certificate[] certs, String authType) {}
+                }
+        };
+
+        SSLContext sslContext = SSLContext.getInstance("TLS");
+        sslContext.init(null, trustAllCerts, new java.security.SecureRandom());
+
+        HttpsURLConnection.setDefaultSSLSocketFactory(sslContext.getSocketFactory());
+        HttpsURLConnection.setDefaultHostnameVerifier((hostname, session) -> true);
+
+        return NimbusJwtDecoder
+                .withIssuerLocation("https://34.116.235.108/realms/garment_print")
+                .build();
     }
 }
